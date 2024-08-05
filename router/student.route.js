baebc2d0d696b53134334286b6a84ed174ffe11d
nerdsuraj <soorajkrpandit@gmail.com>
2024-08-05 14:30:55 +0530

    added the middel ware to the student route

--- router/student.route.js
+++ router/student.route.js
@@ -4,8 +4,8 @@ const studentController = require('../controller/student.controller');
 const helper = require('../utilities/helper');
 
 
-router.get('/student_list', studentController.student_list);
-router.post('/student_create', studentController.student_create_update);
-router.delete('/student_delete/:id', studentController.student_delete);
+router.get('/student_list', helper.checkToken , studentController.student_list);
+router.post('/student_create',helper.checkToken , studentController.student_create_update);
+router.delete('/student_delete/:id',helper.checkToken , studentController.student_delete);
 
 module.exports = router;
--- utilities/helper.js
+++ utilities/helper.js
@@ -7,13 +7,18 @@ const db_query = require("../utilities/db.query");
 let checkToken = (req, res, next) => {
     try {
         let reqBody = JSON.parse(JSON.stringify(req.body));
-        console.log("🚀 ~ checkToken ~ reqBody:", reqBody)
         let token = req.headers["x-access-token"] || req.headers["authorization"];
         if (token.startsWith("Bearer")) {
             token = token.slice(7, token.length);
         }
         if (token) {
             jwt.verify(token, "secretkey", async (err, decoded) => {
+                if (err) {
+                    return res.status(401).json({
+                        status: "failed",
+                        msg: "Invalid Token",
+                    });
+                }
                 let user_email = await db_query.findOne(baseModel.user, { email: decoded.email }, {}, 0);
                 if (user_email) {
                     req.decoded = decoded;
